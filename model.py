9668a9d7743e40b90e1fa5a8ce7ed091e9b0fd9c
sooraj <soorajks2002@gmail.com>
2023-09-25 16:18:10 +0530

    model structure created

---
+++ model.py
@@ -0,0 +1,78 @@
+import torch
+
+names = open('names.txt', 'r').read().splitlines()
+text_data = []
+
+unique_chars = list(set(''.join(names)))
+
+stoi = {char: i+1 for i, char in enumerate(unique_chars)}
+itos = {i+1: char for i, char in enumerate(unique_chars)}
+
+stoi['<>'] = 0
+itos[0] = '<>'
+
+context_window = 3
+
+for name in names:
+    name = ['<>']*context_window + list(name) + ['<>']
+    text_data.append(name)
+
+x_data = []
+y_data = []
+
+for name in text_data:
+    for x1, x2, x3, y in zip(name[:], name[1:], name[2:], name[3:]):
+
+        x1 = stoi[x1]
+        x2 = stoi[x2]
+        x3 = stoi[x3]
+        y = stoi[y]
+
+        x = [x1, x2, x3]
+        x_data.append(x)
+        y_data.append(y)
+
+x_data = torch.tensor(x_data)
+y_data = torch.tensor(y_data)
+
+embedding_size = 4
+embedding_map = torch.randn(size=(27, embedding_size))
+
+x_data = embedding_map[x_data]
+x_data = x_data.view(x_data.shape[0], -1)
+
+n_classes = len(stoi)
+y_data = torch.nn.functional.one_hot(y_data, n_classes).float()
+
+print("x size : ", x_data.shape)
+print("y size : ", y_data.shape)
+
+
+class NeuralNetwork (torch.nn.Module):
+    def __init__(self, input_feature_size):
+        super(NeuralNetwork, self).__init__()
+        self.l1 = torch.nn.Linear(input_feature_size, 50)
+        self.l2 = torch.nn.Linear(50, 27)
+        self.l3 = torch.nn.Softmax()
+
+    def forward(self, x):
+        return self.l2(self.l1(x))
+        return self.l3(self.l2(self.l1(x)))
+
+
+model = NeuralNetwork(x_data.shape[1])
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+
+# print(model(x_data).shape)
+
+epochs = 100
+
+for epoch in range(epochs):
+    outputs = model(x_data)
+    loss = criterion(outputs, y_data)
+    loss.backward()
+    optimizer.step()
+    optimizer.zero_grad()
+
+    print(f"Epoch : {epoch+1}/{epochs} \t Loss : {loss}")
