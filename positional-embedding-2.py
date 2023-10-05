30994a8f1f581717c8736a59e2f3f7bfb9d73b58
sooraj <soorajks2002@gmail.com>
2023-10-05 12:17:14 +0530

    optimized code and fixed torch.sin() error

--- positional-embedding-2.py
+++ positional-embedding-2.py
@@ -1,4 +1,5 @@
 import torch
+import math
 
 names = open('names.txt', 'r').read().splitlines()
 
@@ -33,24 +34,24 @@ class PositionalEmbedding(torch.nn.Module):
     def __init__(self, embedding_size):
         super(PositionalEmbedding, self).__init__()
         self.embedding_size = embedding_size
-        self.i_division_values = 1 / torch.pow(10000, 2 * torch.arange(embedding_size) / embedding_size)
+        i_values = [i-1 if i%2 else i for i in range(embedding_size)]
+        self.i_division_values = 1 / torch.pow(10000, torch.tensor(i_values) / embedding_size)
 
     def forward(self, x):
-        batch, n_positions = x.shape
+        _, n_positions = x.shape
 
-        position_matrix = torch.arange(n_positions, dtype=torch.float).unsqueeze(0)
+        position_matrix = torch.arange(n_positions, dtype=torch.float).unsqueeze(1)
+        
+        positional_embedding = position_matrix * self.i_division_values.unsqueeze(0)
+        
+        X = positional_embedding
+        X[:, 1::2] = torch.cos(X[:, 1::2])
+        X[:, 0::2] = torch.sin(X[:, 0::2])
+                
+        return X
 
-        positional_embedding = position_matrix * self.i_division_values.unsqueeze(1)
 
-        positional_embedding = torch.sin(positional_embedding)  # Applies sin to even indices
-        positional_embedding[:, 1::2] = torch.cos(positional_embedding[:, 1::2])  # Applies cos to odd indices
-
-        # return positional_embedding #return in embedding_size, n_positions formta
-        return positional_embedding.permute(1,0)
-
-
-
-positional_embedder = PositionalEmbedding(10)
+positional_embedder = PositionalEmbedding(5)
 pos_embeddings = positional_embedder(X)
 
-print(pos_embeddings[2], pos_embeddings.shape)
+print(pos_embeddings)
